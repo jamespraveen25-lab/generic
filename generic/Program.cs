9fe639cb7ae90f4932eb53cd3d976310bd37219a
admin <admin@DESKTOP-K4H3P0H>
2026-06-10 15:46:40 +0530

    Add project files.

---
+++ generic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic
+{
+    internal class Program
+    {
+        class student
+        {
+            internal void display<j,k,l,m,n,o> (j sid,k sname,l scollage,m location,n address,o pincode)
+            {
+                Console.WriteLine("student id is" + sid);
+                Console.WriteLine("student name is"+sname);
+                Console.WriteLine("student collage is"+scollage);
+                Console.WriteLine("student loaction is "+location);
+                Console.WriteLine("student address"+address);
+                Console.WriteLine("studnet pincode"+pincode);
+
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            student james = new student();
+            james.display<int,string,string,string,string,int>(25,"james","nawab","amaravathi","vizag",500013);
+        }
+    }
+}
